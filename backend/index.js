bad53cbd4b233562a8eef4b2933b9b22fbbacdd6
ushara <thimalushara@gmail.com>
2021-06-23 12:18:05 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,38 @@
-console.log("hello world, i am ushara!");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+const DBURL = "mongodb://localhost/AFSE2018";
+
+mongoose.connect(
+  DBURL,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (error) => {
+    if (error) {
+      console.log("Database Error : ", error.message);
+    }
+  }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("Database Conneted");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World !!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
